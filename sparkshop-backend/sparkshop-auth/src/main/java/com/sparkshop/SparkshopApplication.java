ae4e56eb77c3b4552dacfee40968ae0f8dcd872e
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2025-08-09 10:54:09 +0530

    feat: add Spring Boot entry point

---
+++ sparkshop-backend/sparkshop-auth/src/main/java/com/sparkshop/SparkshopApplication.java
@@ -0,0 +1,13 @@
+package com.sparkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkshopApplication.class, args);
+	}
+
+}
